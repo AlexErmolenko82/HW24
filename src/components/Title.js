dc207fa12ff2afd4d91b69c3b08b91e6cc72fc52
Alex Ermolenko <alex.ermolenko1@gmail.com>
2022-05-18 17:02:24 +0300

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+
+const Title = ({ name , type }) => {
+    return <p style = {{fontWeight: type}}>Title name is - {name}</p>
+}
+
+Title.propTypes = {
+    name: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(["bold", "normal", ""])
+  };
+  
+  Title.defaultProps = {
+    type: "normal"
+  };
+
+export default Title;
